d951b35f3cbb19f32ef99e099483fbc7a9c31dfb
Julian Abouzeid <Julian@wheresnick.com.au>
2023-09-17 14:33:07 +1000

    added questions and styling

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,7 @@
 var timerEl = document.getElementById('timer');
 var startEl = document.getElementById('startQuizButton');
 
+var currentQuestion = 1
 var timeInterval
 
 timerEl.textContent = "Click the Start Quiz button to begin!";
@@ -20,9 +21,44 @@ function startCountdown() {
         }, 1000);
       }
 
-// Start the timer //
+// Start the timer and the quiz //
 startEl.addEventListener("click", function(event) {
     event.preventDefault();
     clearInterval(timeInterval);
     startCountdown();
+    displayQuestion();
+    startEl.style.display = "none";
+    document.getElementById('question').style.display = "block";
+    document.getElementById('options').style.display = "block";
 });
+
+// Quiz Questions and answers as an array //
+var questions = [
+    {
+        question: "What is a variable?",
+        options: ["someone who is very able at coding", "containers for storing data", "one of many things that could go wrong"],
+        correctAnswer: "b"
+    },
+    {
+        question: "What is a string?",
+        options: ["a line of commands that must be followed", "your cats best friend", "for the storing and manipulating of text"],
+        correctAnswer: "c"
+    },
+    {
+        question: "What does 'else if' used to specify?",
+        options: ["a new condition to test, if the first condition is false", "an offer that you can't refuse", "an inability to choose between two pizza toppings"],
+        correctAnswer: "a"
+    },
+]
+
+function displayQuestion() {
+    var questionData = questions[currentQuestion - 1];
+    document.getElementById('question').innerHTML = "Question " + currentQuestion + ": " + questionData.question;
+    var optionsHtml = '';
+
+    questionData.options.forEach(function (option, index) {
+        optionsHtml += '<label><input type="radio" name="q' + currentQuestion + '" value="' + String.fromCharCode(97 + index) + '"> ' + option + '</label>';
+    });
+
+    document.getElementById('options').innerHTML = optionsHtml;
+}
